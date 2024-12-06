c417db809a0befb8bdafdf59576f9e0013b55445
spiiin <sanya.boyko@gmail.com>
2024-12-06 17:46:44 +0100

    5-6

---
+++ 2024-6/Program.fs
@@ -0,0 +1,105 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+
+type Direction = Up | Down | Left | Right
+
+let turnRight direction =
+    match direction with
+    | Up -> Right
+    | Right -> Down
+    | Down -> Left
+    | Left -> Up
+
+let move (x, y) direction =
+    match direction with
+    | Up -> (x, y - 1)
+    | Down -> (x, y + 1)
+    | Left -> (x - 1, y)
+    | Right -> (x + 1, y)
+
+let parseMap (lines: string[]) =
+    let mutable guardPosition = (0, 0)
+    let mutable guardDirection = Up
+    let obstacles = HashSet<(int * int)>()
+
+    for y in 0 .. lines.Length - 1 do
+        for x in 0 .. lines.[y].Length - 1 do
+            match lines.[y].[x] with
+            | '^' -> guardPosition <- (x, y)
+            | '#' -> obstacles.Add((x, y)) |> ignore
+            | _ -> ()
+    (guardPosition, guardDirection, obstacles)
+
+let simulateGuard (initialPosition, initialDirection, obstacles: #ISet<(int * int)>) (mapWidth, mapHeight) =
+    let visited = HashSet<(int * int)>()
+    let mutable position = initialPosition
+    let mutable direction = initialDirection
+
+    visited.Add(position) |> ignore
+
+    let isInsideMap (x, y) = x >= 0 && x < mapWidth && y >= 0 && y < mapHeight
+    let mutable continueMoving = true
+    while continueMoving do
+        let nextPosition = move position direction
+        if not (isInsideMap (fst nextPosition, snd nextPosition)) || obstacles.Contains(nextPosition) then
+            direction <- turnRight direction
+        else
+            position <- nextPosition
+            visited.Add(position) |> ignore
+    
+        if not (isInsideMap (fst (move position direction), snd (move position direction))) then
+            continueMoving <- false
+
+    visited.Count
+
+let simulateWithObstruction initialPosition initialDirection (obstacles: HashSet<(int * int)>) (mapWidth, mapHeight) obstruction =
+    let visited = HashSet<(int * int * Direction)>()
+    let mutable position = initialPosition
+    let mutable direction = initialDirection
+
+    obstacles.Add(obstruction) |> ignore
+
+    let isInsideMap (x, y) = x >= 0 && x < mapWidth && y >= 0 && y < mapHeight
+
+    let mutable isLoop = false
+    let mutable continueMoving = true
+    while continueMoving && not isLoop do
+        let state = (fst position, snd position, direction)
+        if visited.Contains(state) then
+            isLoop <- true
+        else
+            visited.Add(state) |> ignore
+            let nextPosition = move position direction
+            if not (isInsideMap nextPosition) then
+                continueMoving <- false
+            elif obstacles.Contains(nextPosition) then
+                direction <- turnRight direction
+            else
+                position <- nextPosition
+
+    obstacles.Remove(obstruction) |> ignore
+    isLoop
+
+let findLoopObstructionPositions (initialPosition, initialDirection, obstacles: HashSet<(int * int)>) (mapWidth, mapHeight) =
+    let potentialPositions =
+        [for y in 0 .. mapHeight - 1 do
+            for x in 0 .. mapWidth - 1 do
+                let pos = (x, y)
+                if pos <> initialPosition && not (obstacles.Contains(pos)) then
+                    yield pos]
+
+    potentialPositions
+    |> List.filter (fun pos -> simulateWithObstruction initialPosition initialDirection obstacles (mapWidth, mapHeight) pos)
+    |> List.length
+
+[<EntryPoint>]
+let main argv =
+    let input = File.ReadAllLines("input-2024-6.txt")
+    let mapData = parseMap input
+    let mapSize = input.[0].Length, input.Length
+    let result1 = simulateGuard mapData mapSize
+    printfn "The guard will visit %d distinct positions." result1
+    let result2 = findLoopObstructionPositions mapData mapSize
+    printfn "There are %d positions where an obstruction causes a loop." result2
+    0
---
+++ 2024_5/Program.fs
@@ -0,0 +1,87 @@
+﻿open System
+open System.IO
+
+let parseRules (lines: string list) =
+    lines
+    |> List.map (fun line ->
+        let parts = line.Split('|')
+        int parts.[0], int parts.[1])
+
+let parseUpdates (lines: string list) =
+    lines |> List.map (fun line -> line.Split(',') |> Array.map int |> Array.toList)
+
+let validateUpdate (rules: (int * int) list) (update: int list) =
+    let indices = update |> List.mapi (fun i x -> x, i) |> Map.ofList
+    rules
+    |> List.forall (fun (x, y) ->
+        match Map.tryFind x indices, Map.tryFind y indices with
+        | Some ix, Some iy -> ix < iy
+        | _ -> true)
+
+let middlePage (update: int list) =
+    update.[update.Length / 2]
+
+let reorderUpdate (rules: (int * int) list) (update: int list) =
+    let dependencies = 
+        rules 
+        |> List.filter (fun (x, y) -> List.contains x update && List.contains y update)
+        |> List.groupBy fst
+        |> Map.ofList
+    let visited = System.Collections.Generic.HashSet<int>()
+    let result = System.Collections.Generic.List<int>()
+    let inProcess = System.Collections.Generic.HashSet<int>()
+    
+    let rec visit node =
+        if visited.Contains(node) then ()
+        elif inProcess.Contains(node) then
+            failwithf "Cyclic dependency detected involving page %d" node
+        else
+            inProcess.Add(node) |> ignore
+            match Map.tryFind node dependencies with
+            | Some dependents ->
+                dependents
+                |> List.map snd
+                |> List.iter visit
+            | None -> ()
+            inProcess.Remove(node) |> ignore
+            visited.Add(node) |> ignore
+            result.Add(node)
+
+    update |> List.iter visit
+    result |> Seq.toList
+
+let processInput (rules: (int * int) list) (updates: int list list) =
+    updates
+    |> List.filter (validateUpdate rules)
+    |> List.map middlePage
+    |> List.sum
+
+let processInputPartTwo (rules: (int * int) list) (updates: int list list) =
+    let incorrectlyOrdered = 
+        updates |> List.filter (fun update -> not (validateUpdate rules update))
+    incorrectlyOrdered
+    |> List.map (reorderUpdate rules)
+    |> List.map middlePage
+    |> List.sum
+
+let span predicate (list: 'a list) =
+    let before = list |> List.takeWhile predicate
+    let after = list |> List.skipWhile predicate
+    before, after
+
+let readInput fileName =
+    let lines = File.ReadAllLines(fileName) |> Array.toList
+    let ruleLines, updateLines =
+        lines |> span (fun line -> line.Contains("|"))
+    let rules = parseRules ruleLines
+    let updates = parseUpdates (List.tail updateLines)
+    rules, updates
+
+[<EntryPoint>]
+let main argv =
+    let rules, updates = readInput "input-2024-5.txt"
+    let resultPartOne = processInput rules updates
+    printfn $"The sum of middle page numbers from the file (Part One): {resultPartOne}" 
+    let resultPartTwo = processInputPartTwo rules updates
+    printfn $"The sum of middle page numbers from corrected updates (Part Two): {resultPartTwo}" 
+    0
