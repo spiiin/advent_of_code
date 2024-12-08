b6bc4e3d438a93d7e44abb1bcaa743db12a0f8c7
spiiin <sanya.boyko@gmail.com>
2024-12-08 14:43:02 +0100

    more functional day 8

--- 2024_8/Program.fs
+++ 2024_8/Program.fs
@@ -1,40 +1,54 @@
 ﻿open System
-open System.Collections.Generic
 open System.IO
+open System.Collections.Generic
 
-let findAntinodes map part2 =
-    let height = List.length map
-    let width = List.length (List.head map)
-    let antennas = 
-        [ for y in 0 .. height - 1 do
-            for x in 0 .. width - 1 do
-                if map.[y].[x] <> '.' then
-                    yield (x, y, map.[y].[x]) ]
-    let antinodes = HashSet<int * int>()
-    let addAntinodesFromLine (x, y, dx, dy) =
-        let mutable x, y = x + dx, y + dy
-        while x >= 0 && x < width && y >= 0 && y < height do
-            antinodes.Add((x, y)) |> ignore
-            x <- x + dx
-            y <- y + dy
-    for i in 0 .. List.length antennas - 1 do
-        for j in 0 .. List.length antennas - 1 do
-            if i <> j then
-                let (x1, y1, freq1) = antennas.[i]
-                let (x2, y2, freq2) = antennas.[j]
-                if freq1 = freq2 then
-                    let dx, dy = x2 - x1, y2 - y1
-                    if part2 then addAntinodesFromLine (x1, y1, dx, dy); addAntinodesFromLine (x2, y2, -dx, -dy)
-                    else
-                        let xAntinode1, yAntinode1 = x1 - dx, y1 - dy
-                        let xAntinode2, yAntinode2 = x2 + dx, y2 + dy
-                        if xAntinode1 >= 0 && xAntinode1 < width && yAntinode1 >= 0 && yAntinode1 < height then
-                            antinodes.Add((xAntinode1, yAntinode1)) |> ignore
-                        if xAntinode2 >= 0 && xAntinode2 < width && yAntinode2 >= 0 && yAntinode2 < height then
-                            antinodes.Add((xAntinode2, yAntinode2)) |> ignore
-    antinodes.Count
+type Position = int * int
 
-let filePath = "input-2024-8.txt"
-let map = File.ReadLines(filePath) |> Seq.map (fun line -> line.ToCharArray() |> List.ofArray) |> Seq.toList
-printfn $"Part 1: Total unique antinode locations: {findAntinodes map false}"
-printfn $"Part 2: Total unique antinode locations: {findAntinodes map true}"
+let parseInput (lines: string seq) =
+    lines
+    |> Seq.mapi (fun y row -> row |> Seq.mapi (fun x c -> c, (x, y)))
+    |> Seq.collect id
+    |> Seq.filter (fst >> ((<>) '.'))
+    |> Seq.groupBy fst
+    |> Seq.map (fun (freq, positions) -> freq, positions |> Seq.map snd |> Seq.toList)
+    |> Seq.toList
+
+let isInsideMap (width, height) (x, y) =
+    x >= 0 && x < width && y >= 0 && y < height
+
+let findAntinodesPure (width, height) (x1, y1) (x2, y2) =
+    let dx, dy = x2 - x1, y2 - y1
+    [ (x1 - dx, y1 - dy); (x2 + dx, y2 + dy) ]
+    |> List.filter (isInsideMap (width, height))
+
+let findAntinodesHarmonic (width, height) (x1, y1) (x2, y2) =
+    let dx, dy = x2 - x1, y2 - y1
+    let rec generateAntinodes x y dx dy acc =
+        if isInsideMap (width, height) (x, y) then
+            generateAntinodes (x + dx) (y + dy) dx dy ((x, y) :: acc)
+        else acc
+    let forward = generateAntinodes x1 y1 dx dy []
+    let backward = generateAntinodes x2 y2 -dx -dy []
+    forward @ backward
+
+let countAntinodes lines part2 =
+    let antennas = parseInput lines
+    let width, height = String.length (Seq.head lines), Seq.length lines
+    let antinodeSet = HashSet<Position>()
+    for _, positions in antennas do
+        for i = 0 to List.length positions - 1 do
+            for j = i + 1 to List.length positions - 1 do
+                let (x1, y1), (x2, y2) = positions.[i], positions.[j]
+                let antinodes =
+                    if part2 then findAntinodesHarmonic (width, height) (x1, y1) (x2, y2)
+                    else findAntinodesPure (width, height) (x1, y1) (x2, y2)
+                for antinode in antinodes do
+                    antinodeSet.Add antinode |> ignore
+    antinodeSet.Count
+
+[<EntryPoint>]
+let main _ =
+    let lines = File.ReadAllLines("input-2024-8.txt")
+    printfn $"Part 1: {countAntinodes lines false}"
+    printfn $"Part 2: {countAntinodes lines true}"
+    0
